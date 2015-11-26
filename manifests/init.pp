17814c48edb3f33b443442e4f263d4dedebf939d
Reid Vandewiele <reid@puppetlabs.com>
2015-11-25 22:27:57 -0800

    Revert "Change path to use backslashes"
    This reverts commit 336b2090e78723950d782ce83a6a373468f0185c.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 class temp217 {
 
   exec { 'update nimbus':
-    path    => 'C:\\Program Files\\Puppet Labs\\Puppet\\bin',
-    command => 'puppet module install tse/nimbus --force --version 0.6.2 --modulepath C:/ProgramData/PuppetLabs/code/environments/production/modules',
+    path    => 'C:/Program Files/Puppet Labs/Puppet/bin',
+    command => 'puppet module install tse/nimbus --force --version 0.6.2',
     unless  => "puppet module list | FindStr /r 'tse-nimbus.*0.6.2'",
   }
 
