c2e9277d175256ff5473755e4b709e598a3496d5
Reid Vandewiele <reid@puppetlabs.com>
2015-11-28 16:57:47 -0800

    Update version of nimbus referenced

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,8 @@ class temp217 {
 
   exec { 'update nimbus':
     path    => 'C:/Windows/system32;C:/Program Files/Puppet Labs/Puppet/bin',
-    command => 'puppet.bat module install tse/nimbus --force --version 0.6.2',
-    unless  => 'cmd.exe /c puppet.bat module list | FindStr /r tse-nimbus.*0.6.2',
+    command => 'puppet.bat module install tse/nimbus --force --version 0.6.3',
+    unless  => 'cmd.exe /c puppet.bat module list | FindStr /r tse-nimbus.*0.6.3',
   }
 
 }
